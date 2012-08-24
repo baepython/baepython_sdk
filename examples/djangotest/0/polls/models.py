bb6ad3fa7020ed5db6dc3b7c5c2ac14eb266b1a9
chenyf <rstevens2008@hotmail.com>
2012-08-24 11:06:37 +0800

    add djangotest

---
+++ examples/djangotest/0/polls/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+import datetime
+from django.utils import timezone
+
+# Create your models here.
+
+
+class Poll(models.Model):
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __unicode__(self):
+        return self.question
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now()-datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll)
+    choice = models.CharField(max_length=200)
+    votes = models.IntegerField()
+
+    def __unicode__(self):
+        return self.choice
+
+
---
+++ examples/djangotest/0/polls/views.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from django.shortcuts import get_object_or_404, render_to_response
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from django.template import RequestContext
+from polls.models import Choice, Poll
+
+
+def vote(request, poll_id):
+    p = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render_to_response('polls/detail.html', 
+            {
+                'poll': p,
+                'error_message': "You didn't select a choice",
+            },
+            context_instance=RequestContext(request))
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('poll_results', args=(p.id,)))
+
